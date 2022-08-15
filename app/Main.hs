9f833854e5ff703bb3a98acb56a87d3abff72992
Laurence Warne <laurencewarne@gmail.com>
2022-08-15 21:28:20 +0100

    Grab comments

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
 import Lib
 import Reddit
-import Reddit.Types.Post
+import Reddit.Types.Post hiding (score)
 import Reddit.Types.Subreddit as R
 import Control.Monad.IO.Class
 import Reddit.Types.SearchOptions (Order(Relevance))
 import Data.Either
-import Data.Maybe (listToMaybe)
+import Data.Maybe (listToMaybe, catMaybes, mapMaybe)
+import Reddit.Types.Comment
+import Control.Monad (mfilter)
+import Data.Text (Text)
 
-getTopPosts :: MonadIO m => m (Either (APIError RedditError) [Post])
-getTopPosts = runRedditAnon $ do
+
+getTopWeeklyComments :: MonadIO m => m (Either (APIError RedditError) [Text])
+getTopWeeklyComments = runRedditAnon $ do
   let options = Options Nothing Nothing
-  Listing _ _ posts <- search (Just $ R "emacs") options Relevance "org"
-  return posts
+  let s = "Weekly Tips"
+  Listing _ _ posts <- search (Just $ R "emacs") options Relevance s
+  comments <- traverse (getComments . postID) posts
+  let allComments = mapMaybe (\case
+                      Actual comment -> if any (> 20) (score comment) then
+                                          Just $ body comment
+                                        else Nothing
+                      _ -> Nothing) (mconcat comments)
+  return allComments
 
 main :: IO ()
 main = do
-  posts <- getTopPosts
+  posts <- getTopWeeklyComments
   let firstPost = listToMaybe (fromRight [] posts)
   liftIO $ print firstPost
