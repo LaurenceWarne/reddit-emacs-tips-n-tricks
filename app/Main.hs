20ed5852da1c880e32afc3a15c274ba57f2f484e
Laurence Warne <laurencewarne@gmail.com>
2023-08-20 20:29:58 +0100

    Don't write when zero comments found

--- app/Main.hs
+++ app/Main.hs
@@ -14,9 +14,11 @@ main = do
   print ("No posts: " <> show (length posts))
   commentsOrErr <- getTopWeeklyComments posts
   let comments = sortOn (negate . upvotes) (fromRight [] commentsOrErr)
+  let totalComments = length comments
 
-  print ("No comments: " <> show (length comments))
   let s = foldMap ((<> "\n\n") . commentInfoToMd) comments
-  writeFile "out.md" s
+  if totalComments > 0 then
+    writeFile "out.md" s *> print ("Processed comments: " <> show totalComments)
+  else print ("Skipped writing comments as none were found" :: [Char])  -- Why needed?
   --let s2 = foldMap ((<> "\n\n") . comment) comments
   --writeFile "out2.md" s2
