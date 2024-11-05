086782382a06cd0fb3f015d3263d2933cb50a1e9
Laurence Warne <laurencewarne@gmail.com>
2024-11-05 20:31:48 +0000

    Fix html escape codes appearing in output

--- bin/run.py
+++ bin/run.py
@@ -3,7 +3,7 @@ Usage: python3 bin/run.py [--all] [--skip-pushing] [--tmp-directory]
 
 https://www.reddit.com/dev/api/#GET_search
 """
-import sys, itertools, json, logging, os, datetime, collections
+import sys, itertools, json, logging, os, datetime, collections, re, html
 
 import requests
 from collections import namedtuple
@@ -26,9 +26,18 @@ GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 AUTH_URL = "https://www.reddit.com/api/v1/access_token"
 BASE_EMACS_SUB_URL = "https://oauth.reddit.com/r/emacs"
 
-MarkdownType = collections.namedtuple("MarkdownType", "header code_start code_end link bold ext")
-GithubMD = MarkdownType("##", "```", "```", lambda text, link: f"[{text}]({link})", lambda s: f"**{s}**", "md")
-OrgMD = MarkdownType("**", "#+BEGIN_SRC elisp", "#+END_SRC", lambda text, link: f"[[{link}][{text}]]", lambda s: f"*{s}*", "org")
+MarkdownType = collections.namedtuple("MarkdownType", "header code_start code_end link bold ext single_line_code bof")
+GithubMD = MarkdownType("##", "```", "```", lambda text, link: f"[{text}]({link})", lambda s: f"**{s}**", "md", "`", "")
+OrgMD = MarkdownType(
+    header = "**",
+    code_start = "#+BEGIN_SRC elisp",
+    code_end = "#+END_SRC",
+    link = lambda text, link: f"[[{link}][{text}]]",
+    bold = lambda s: f"*{s}*",
+    ext = "org",
+    single_line_code = "~",
+    bof = "#+OPTIONS: toc:nil\n"
+)
 
 def get_headers(token=None):
     return {
@@ -61,16 +70,14 @@ def get_comments(token, read_all):
         if post["title"].strip().lower().startswith("weekly tips")  # Early were called 'Weekly tips/trick/etc/ thread'
     ]
     posts, after = search_emacs_subreddit(headers, 0)
-    # for p in posts: print(p["title"])
     total_posts, calls = len(posts), 1
     all_comments = comments_from_posts(posts)
     log = lambda : LOGGER.info("Completed API call %d, posts: %d, new offset: %s, total_comments: %d", calls, len(posts), after, len(all_comments))
     log()
-    if not read_all: return comments
+    if not read_all: return all_comments
     while after:
         calls += 1
         posts, after = search_emacs_subreddit(headers, total_posts, after)
-        # for p in posts: print(p["title"])
         total_posts += len(posts)
         all_comments += comments_from_posts(posts)
         log()
@@ -116,9 +123,12 @@ def comment_to_md(content, username, post_id, comment_id, upvotes, md_type=Githu
         last_formatting = new_formatting
 
     quote_last_line = last_formatting == indented
-    split = (markdown + ("\n```" if quote_last_line else "")).split("```\n")
-    code_it = itertools.cycle([md_type.code_start + "\n", md_type.code_end + "\n"])
-    return title + "".join([a + b for a, b in zip(split[:-1], code_it)] + [split[-1]])
+    split = (markdown + ("\n```" if quote_last_line else "")).split("```")
+    code_it = itertools.cycle([md_type.code_start, md_type.code_end])
+    md = "".join([a + b for a, b in zip(split[:-1], code_it)] + [split[-1]])
+    single_line_code_converted = re.sub(r"(?<!`)(`)(?!`)", md_type.single_line_code, md)
+    heading_escaped = re.sub(r"^(\*+)", r"\\\1", md, flags=re.M)
+    return title + html.unescape(heading_escaped)
 
 
 def init_git_repo():
@@ -193,12 +203,13 @@ def run(all_posts, skip_pushing, tmp_directory, md_type=OrgMD):
     LOGGER.info("Of which %d are new", new_comments)
 
     for comment, attribs in comments.items(): existing_comments[comment] = attribs
+    LOGGER.info("Giving a total of %d comments", len(existing_comments))
     with open(FILE, "w") as f:
         f.write(json.dumps(existing_comments, default=str))
 
     s = "\n\n".join(t[1]["body"] for t in sorted(existing_comments.items(), key=lambda t: t[1]["upvotes"], reverse=True))
     with open(f"{OUT}.{md_type.ext}", "w") as f:
-        f.write(s)
+        f.write(md_type.bof + s)
 
     if skip_pushing:
         LOGGER.info("Skipping pushing to git repo")
